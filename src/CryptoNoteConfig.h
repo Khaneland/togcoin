d0d9c9be3abe564a3eca9c2eb1266acfca835488
Guilherme <email.guilherme.t@gmail.com>
2019-02-07 19:27:07 +0000

    incluidos enderecos seed

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "togcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000194d6dfe592e901029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121018c69cd9e26872755e38ee928514bf06d8e2efca2fdeb62ff3d7ec57ebc93b326";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "your_seed_ip1.com:8080",
-  "your_seed_ip2.com:8080",
+  "172.17.0.3:8080",
+  "172.17.0.4:8080",
 };
 
 struct CheckpointData {
